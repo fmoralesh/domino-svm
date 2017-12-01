b92282167781da1bb4713da09c2935b6902a8080
Fabio Morales <fabmoraleshidalgo@gmail.com>
2017-12-01 09:44:09 -0400

    square.cpp and square.h
    Se añadieron las funciones para reconocer rectangulos en una imagen, util para reconocer las fichas de domino de la foto.

--- main.cpp
+++ main.cpp
@@ -11,15 +11,20 @@
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
+#include "squares.h"
+
 int main() {
     // Reading the image.
-    cv::Mat img = cv::imread("./data/img_prueba_2_4.jpg",CV_LOAD_IMAGE_COLOR);
+    Mat img = imread("./data/img_prueba_2_4.jpg",CV_LOAD_IMAGE_COLOR);
 
     // Resizing the image.
-    cv::Mat img_resized(img.size().height/4,img.size().width/4, CV_8UC3, cv::Scalar(255,255,255));
+    Mat img_resized(img.size().height/4,img.size().width/4, CV_8UC3, Scalar(255,255,255));
     resize(img, img_resized, img_resized.size(), 0, 0, cv::INTER_CUBIC);
     
-    imshow("Display photo", img_resized);
+    std::vector<vector<Point> > squares;
+    
+    findSquares(img_resized, squares);
+    drawSquares(img_resized, squares);
 
     cv::waitKey(0);
     return 0;
---
+++ squares.h
@@ -0,0 +1,26 @@
+/**********************************************
+ * FILE NAME: squares.h                       *
+ * DESCRIPTION: detects squares in an image   *
+ * AUTHORS: alyssaq/opencv                    *
+ *                                            *
+ *********************************************/
+
+#ifndef SQUARES_H
+#define SQUARES_H
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <dirent.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+void help();
+double angle( Point pt1, Point pt2, Point pt0 );
+void findSquares( const Mat& image, vector<vector<Point> >& squares );
+void drawSquares( Mat& image, const vector<vector<Point> >& squares );
+
+#endif
